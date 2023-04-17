c7a75fb3ecf7fb51dd83e2f4dbb30fd9124c929b
ssakone <abdramanesakone1@gmail.com>
2023-04-17 09:51:53 +0200

    ♻️ Remove no need code

--- src/posometrecamera.h
+++ src/posometrecamera.h
@@ -11,14 +11,7 @@ class PosometreCamera : public QObject
 public:
     explicit PosometreCamera(QObject *parent = nullptr);
     ~PosometreCamera();
-    Q_INVOKABLE void takeValue();
     Q_INVOKABLE float getGamma(QQuickItemGrabResult *data = nullptr);
-    static void setMute(bool mute);
-
-public slots:
-    void getImageGamma(int id, QImage im);
-private:
-    QImageCapture *imageCapture;
 
 signals:
     void lighnessValue(float);
